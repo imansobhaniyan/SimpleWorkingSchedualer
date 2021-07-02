5394b182d59b4d7aaa91a70358f34b2588ff7f51
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2021-07-02 17:54:50 +0430

    fix page references

--- SimpleWorkingSchedualer/ClientApp/src/app/app.module.ts
+++ SimpleWorkingSchedualer/ClientApp/src/app/app.module.ts
@@ -5,10 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CounterComponent } from './pages/counter/counter.component';
+import { FetchDataComponent } from './pages/fetch-data/fetch-data.component';
 import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
