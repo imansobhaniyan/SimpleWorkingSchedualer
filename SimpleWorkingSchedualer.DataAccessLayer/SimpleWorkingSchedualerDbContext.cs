bce0fc268cb9152a0076cc55e53d7975a5a74252
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2021-07-02 23:18:34 +0430

    add db context

---
+++ SimpleWorkingSchedualer.DataAccessLayer/SimpleWorkingSchedualerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWorkingSchedualer.DataAccessLayer
+{
+    public class SimpleWorkingSchedualerDbContext : DbContext
+    {
+        public SimpleWorkingSchedualerDbContext(DbContextOptions<SimpleWorkingSchedualerDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<StorageModels.User> Users { get; set; }
+    }
+}
